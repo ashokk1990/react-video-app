4204e7651e2c0c23d91d4827b1b85ef2bbc133d8
ashokk1990 <ashok.mac3@gmail.com>
2019-01-28 09:53:13 -0800

    Moved api key to local config file

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
-const key = "AIzaSyBb5VFlrwdTq8ZLNKJRrQWE-3hZvTMIcQY"
+import config from '../../config'
+
 export default axios.create({
     baseURL:'https://www.googleapis.com/youtube/v3',
     params: {
         part:'snippet',
         maxResult:5,
-        key:key
+        key: config.youtube_api_key
     }
 })
